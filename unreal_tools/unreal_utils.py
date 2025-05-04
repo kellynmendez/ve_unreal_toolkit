4003d4713178e7ca78169ff2a701ff2803f0d9fe
Kellyn Mendez <kpm.mendez@gmail.com>
2025-05-03 19:27:26 -0500

    Revert "restructure and stuff"
    This reverts commit b3fe2b7319a5c359c5824ad3884e197fa5950057.

--- unreal_tools/unreal_utils.py
+++ unreal_tools/unreal_utils.py
@@ -15,12 +15,13 @@
 # ----------------------------------------------------------------------------- IMPORTS --#
 
 # Built-in
+import sys
 
 # Third party
+import unreal
 from PySide6 import QtWidgets, QtCore
 
 # Internal
-from unreal_tools.utils.gui_utils import get_qt_app, parent_gui_to_unreal
 
 # External
 
@@ -28,10 +29,51 @@ from unreal_tools.utils.gui_utils import get_qt_app, parent_gui_to_unreal
 # ----------------------------------------------------------------------------------------#
 # --------------------------------------------------------------------------- FUNCTIONS --#
 
+def parent_gui_to_unreal(gui):
+    """
+    Parents the PyQt or PySide window to the Unreal editor.
+    The window will always sit on top of the editor and will be
+    minimized along with it.
+
+    :param gui: The PyQt or PySide to affect
+    :type: QtWidgets.QWidget
+    """
+    unreal.parent_external_window_to_slate(int(gui.winId()))
+
+def get_qt_app():
+    """
+    Lazy-load the PyQt6 application.
+
+    :return: The application
+    :type: QtWidgets.QApplication
+    """
+    app = QtWidgets.QApplication.instance()
+
+    if not QtWidgets.QApplication.instance():
+        app = _create_qt_app()
+
+    return app
+
+def _create_qt_app():
+    """
+    Creates the default PyQt6 application.
+
+    :return: The application
+    :type: QtWidgets.QApplication
+    """
+    if not QtWidgets.QApplication.instance():
+        app = QtWidgets.QApplication(sys.argv)
+    else:
+        app = QtWidgets.QApplication.instance()
+    window = QtWidgets.QWidget()
+    window.show()
+    app.exec_()
+    return app
+
 # ----------------------------------------------------------------------------------------#
 # ----------------------------------------------------------------------------- CLASSES --#
 
-class CreateFolderHierGUI(QtWidgets.QDialog):
+class CreateFileHierarchyGUI(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         # The tree view
@@ -98,7 +140,7 @@ class CreateFolderHierGUI(QtWidgets.QDialog):
         global file_hier_gui
 
         file_hier_gui = __class__()
-        file_hier_gui.init_gui()
+        file_hier_gui.show_gui()
 
         parent_gui_to_unreal(file_hier_gui)
 
@@ -165,11 +207,9 @@ class CreateFolderHierGUI(QtWidgets.QDialog):
         :type: QEvent
         """
         if received == self.folder_tree.viewport():
-            # If it was a click, see if what was clicked was an item
-            #   Otherwise, deselect all rows
+            # If it was a click, see if what was clicked was an item.
+            #   Otherwise, deselect all rows.
             if event.type() == QtCore.QEvent.MouseButtonPress:
                 clicked = self.folder_tree.indexAt(event.pos())
                 if not clicked.isValid():
                     self.folder_tree.clearSelection()
-
-        return True
